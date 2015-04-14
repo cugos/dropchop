255cdbbf3698d2af7ea08ce7144d19311b717f07
svmatthews <matthews.sam@gmail.com>
2015-04-13 20:53:09 -0700

    mergning in refactor/menus

--- dist/js/L.DNC.js
+++ dist/js/L.DNC.js
@@ -598,3 +598,48 @@ L.DNC.TurfOperation = L.DNC.Operation.extend({
     }
 
 });
+
+L.DNC = L.DNC || {};
+L.DNC.Notifications = L.Class.extend({
+
+
+    statics: {},
+
+    // defaults
+    options: {},
+
+    initialize: function (map, options) {
+
+        // override defaults with passed options
+        L.setOptions(this, options);
+        this._map = map;
+
+        // create notification center & locations
+        this.hub = document.getElementById('notifications');
+
+    } ,
+
+    // used to add a notification to the DOM
+    add: function ( options ) {
+        // add a new notification to the stream
+        noteCenter = this.hub;
+
+        var note = document.createElement('div');
+        note.className = 'notification ' + options.type;
+        note.innerHTML = options.text;
+        noteCenter.appendChild(note);
+        
+        // TODO: add/remove notifications to an array to interact with them
+        // instead of relying on setTimeout() dictating their existence.
+        setTimeout(function () {
+            noteCenter.removeChild( noteCenter.firstChild );
+        }, 4000);
+
+    }
+
+    // TODO: add event listeners for notifications here instead of 
+    // throughout the application. Maybe?
+
+
+
+});
