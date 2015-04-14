f2552711356cc1f9c1160e29baa4f5f763429cc4
Anthony Lukach <anthonylukach@gmail.com>
2015-04-13 19:55:17 -0600

    Put filereader handler into file reader class

--- src/js/L.DNC.js
+++ src/js/L.DNC.js
@@ -16,17 +16,6 @@
             this.dropzone = new L.DNC.DropZone( this.mapView._map, {} );
             this.layerlist = new L.DNC.LayerList( { layerContainerId: 'dropzone' } ).addTo( this.mapView._map );
             this.menuBar = new L.DNC.MenuBar( this.layerlist, {} );
-
-            // examples of events that L.DNC.DropZone.FileReader throws
-            this.dropzone.fileReader.on( "fileparsed", function(e){
-                // TODO: This should be refactored so that this.dropzone and
-                // this.layerlist are not so tightly coupled. The logic behind
-                // this tooling should exist within their respective modules.
-                console.debug( "[ FILEREADER ]: file parsed > ", e.file );
-                var mapLayer = L.mapbox.featureLayer(e.file);
-                this.layerlist.addLayerToList( mapLayer, e.fileInfo.name, true );
-                this.numLayers++;
-            }.bind(this));
         };
     }
 })();
