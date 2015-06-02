9557f696b8c02c9b1cbadb479097c9cadfae4376
mapsam <matthews.sam@gmail.com>
2015-06-01 22:56:33 -0700

    adding test file, todo for future!

---
+++ spec/suites/L.DNC.File.js
@@ -0,0 +1,15 @@
+describe("L.DNC.FileExecute Operations", function(){
+	var menu;
+	var ops;
+	var exampleData = true;
+
+	// TODO: test for geojson save
+	describe('Save as a geojson', function () {
+		expect(exampleData).to.eql(true);
+	});
+
+	// TODO: test for shapefile save
+	describe('Save as a shapefile', function () {
+		expect(exampleData).to.eql(true);
+	});
+});
