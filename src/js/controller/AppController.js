bcc4f3cbeb2915d4d4a65749e1655e0157b1b218
Anthony Lukach <anthonylukach@gmail.com>
2015-07-01 23:54:41 -0600

    Correctly set scope (fixes #158)

--- src/js/controller/AppController.js
+++ src/js/controller/AppController.js
@@ -90,7 +90,7 @@ L.Dropchop.AppController = L.Class.extend({
     */
     _addEventHandlers: function(){
         this.dropzone.fileReader.on( 'fileparsed', this._handleParsedFile.bind( this ) );
-        this.fileOpsConfig.executor.on( 'uploadedfiles', this.dropzone.fileReader._handleFiles.bind(this) );
+        this.fileOpsConfig.executor.on( 'uploadedfiles', this.dropzone.fileReader._handleFiles.bind( this.dropzone.fileReader ) );
 
         // Handle clicks on items within menus
         // NOTE: This is where an operation is tied to a menu item
